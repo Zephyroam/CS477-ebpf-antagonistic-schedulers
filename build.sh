e57f063de67b35a40ace65a22032535535271253
Xiangyu Shi <sxyu.shi@gmail.com>
2024-11-13 16:59:35 +0100

    add more scx

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@
 bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
 
 # Compile the scheduler
-clang -target bpf -g -O2 -c sched_ext.bpf.c -o sched_ext.bpf.o -I.
+clang -target bpf -g -O2 -c scx_minimal.bpf.c -o scx_minimal.bpf.o -I.
+clang -target bpf -g -O2 -c scx_central.bpf.c -o scx_central.bpf.o -I.
+clang -target bpf -g -O2 -c scx_simple.bpf.c -o scx_simple.bpf.o -I.
--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Get the name of the scheduler from arguments
+SCHEDULER=$1
+
 # Register the scheduler
-bpftool struct_ops register sched_ext.bpf.o /sys/fs/bpf/sched_ext || (echo "Error attaching scheduler, consider calling stop.sh before" || exit 1)
+bpftool struct_ops register $SCHEDULER.bpf.o /sys/fs/bpf/sched_ext || (echo "Error attaching scheduler, consider calling stop.sh before" || exit 1)
 
 # Print scheduler name, fails if it isn't registered properly
 cat /sys/kernel/sched_ext/root/ops || (echo "No sched-ext scheduler installed" && exit 1)
