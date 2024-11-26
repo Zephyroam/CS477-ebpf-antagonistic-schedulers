5e8d9653c22ce0dbc7ed39f24d5750e206f27a54
Xiangyu Shi <sxyu.shi@gmail.com>
2024-11-26 16:21:41 +0100

    update

--- new/build.sh
+++ new/build.sh
@@ -2,4 +2,4 @@ clang -target bpf -g -O2 -c scx_simple.bpf.c -o scx_simple.bpf.o -I.
 
 bpftool gen skeleton scx_simple.bpf.o > scx_simple.bpf.skel.h
 
-gcc -g -O2 -Wall scx_simple.c -o scx_simple -I/usr/include -lbpf -lelf
+gcc -g -O2 -Wall scx_simple.c -o scx_simple -I/usr/include -lbpf -lelf -I.
