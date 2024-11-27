5de7410c405c7be757283e3667cc3624ab00b124
Xiangyu Shi <sxyu.shi@gmail.com>
2024-11-27 20:57:26 +0100

    update

--- new/build.sh
+++ new/build.sh
@@ -1,2 +1,2 @@
-clang -O2 -target bpf -c cache_misses.bpf.c -o cache_misses.bpf.o
-gcc -o cache_misses main.c -lbpf
+clang -O2 -target bpf -c cache_misses.bpf.c -o cache_misses.bpf.o -I.
+gcc -o cache_misses main.c -lbpf -I.
