377190ca49fb8fbda26ac224fe562a52812cd211
Xiangyu Shi <sxyu.shi@gmail.com>
2024-12-05 16:45:14 +0100

    update

--- synthetic/rocksdb/build/src/CMakeFiles/shinjuku.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for shinjuku.
-# This may be replaced when dependencies are built.
--- synthetic/rocksdb/build/src/CMakeFiles/shinjuku.dir/main.cc.o.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/shinjuku.dir/main.cc.o: \
-  /Users/zephyroam/Documents/CS-477\ Advanced\ operating\ systems/Project/CS-477-Project/synthetic/rocksdb/src/main.cc
