4fcc4fbccd9c30bbd6ccc9e45d57950e120633cc
Xiangyu Shi <sxyu.shi@gmail.com>
2024-12-20 23:19:34 +0100

    clean

--- scx_kun.c
+++ scx_kun.c
@@ -1,9 +1,3 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-/*
- * Copyright (c) 2022 Meta Platforms, Inc. and affiliates.
- * Copyright (c) 2022 Tejun Heo <tj@kernel.org>
- * Copyright (c) 2022 David Vernet <dvernet@meta.com>
- */
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
